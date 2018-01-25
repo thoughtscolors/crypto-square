9d9768617ea81ae2780aa671445401fdc4ff8f92
Brady <vanderbannond@gmail.com>
2018-01-25 11:41:48 -0800

    got the normalized string, working on the math to create a square of 7x8

--- crypto-square/crypto-square.js
+++ crypto-square/crypto-square.js
@@ -12,18 +12,36 @@
 // if less than 54 in length, distribute spaces at end of lines evenly to keep block shape
 // When looping, use similar function to simpleCipher encode to encode letters
 
+
+// var string = 's#$%^&plunk';
+// var cleanString = string.replace(/[^A-Za-z]+/, "");
+// console.log(cleanString);
+
 class Crypto {
-  constructor(str) {
+  constructor(string) {
+    // let cleanArray = [];
+    // let cleanString = ''
 // This creates a rows key and populates the rows with each string line and turns them into numbers with separate indexes in each array.
-    cleanString = str.replace("[^A-Za-z]+", "").split().map(function(arr) {
-      return arr.map(function(e) {
-        return arr;
-      });
-    });
+    this.cleanString = string.replace(/[^A-Za-z]/gi, "").toLowerCase();
+    for (var i = 0; i < this.cleanString.length; i++) {
+    this.r = this.cleanString.length % i;
+    this.c = this.cleanString.length % i;
+    if (this.c >= this.r && (this.c - this.r) <= 1)
+    
+    console.log(this.c);
+    console.log(this.r);
+  }
+    // .map(function(arr) {
+    //   return arr.map(function(e) {
+    //     return arr;
+    //   });
+    // });
   }
 }
-var squareCrypto = new Crypto('s#$%^&plunk')
-console.log(squareCrypto);
+var splunk = new Crypto('S#$%^&plunk')
+console.log(splunk);
+var long = new Crypto("If man was meant to stay on the ground, god would have given us roots.");
+console.log(long);
 
     // This makes a columns key and fills it with empty arrays at each index from the length each row
     // this.cols = new Array(this.rows[0].length).fill().map(function() {
